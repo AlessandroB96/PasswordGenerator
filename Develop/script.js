9d9d1d6fb1b06a7e4235a4a76ef3107f7b48a6f8
Alessandro Belaj <abelaj7196@gmail.com>
2022-01-07 17:59:18 -0500

    while loop created if user inputs wrong password length

--- Develop/script.js
+++ Develop/script.js
@@ -5,40 +5,51 @@ function generatePassword(passwordLength, ) {
 
   // logic
 
-  return generatePassword
+  return generatePassword;
 
-console.log("Generate password called")
 }
- var alphabetLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+ const alphabetLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
- var alphabetCapital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ const alphabetCapital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
- var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+ const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
- var symbols = ["!",""];
+ const symbols = ["!",""];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
 
 function writePassword() {
   console.log("Button was clicked")
-  var passwordText = document.querySelector("#password");
-  
-  var passwordLength = window.prompt("How many characters should your password be? Choose between a minimum of 8 characters and a maximum of 128 characters");
-  
-  //logs the password length of the user's input
-  console.log(passwordLength);     
-  
-  var hasUpperCase = window.prompt("Would you like to include uppercase letters? Type YES or NO")
+  let passwordText = document.querySelector("#password");
+
+  // initialize as false and negate if actually true in while loop
+  let passwordLengthCorrect = false;    
+  let passwordLength = undefined;
+
+  //while this value is not false, the loop will run indefinetely
+  while (passwordLengthCorrect != true) {
+    passwordLength = window.prompt("How many characters should your password be? Choose between a minimum of 8 characters and a maximum of 128 characters");
+    
+    if (passwordLength < 8) {
+      alert("Password must be at least 8 characters");
+    } else if (passwordLength > 128) {
+      alert("Password must be at most 128 characters");
+    } else {
+      passwordLengthCorrect = true;
+    }
+  }
+
+  let hasUpperCase = window.prompt("Would you like to include uppercase letters? Type YES or NO")
   console.log(hasUpperCase);
 
-  var hasSpecialCharacters = window.prompt("Would you like to have special characters included? Type YES or NO");
+  let hasSpecialCharacters = window.prompt("Would you like to have special characters included? Type YES or NO");
   console.log(hasSpecialCharacters);
   
-  var password = generatePassword(passwordLength,hasUpperCase,hasSpecialCharacters);
+  let password = generatePassword(passwordLength,hasUpperCase,hasSpecialCharacters);
 
   passwordText.value = password;
   
